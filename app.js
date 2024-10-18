afcc03158940eaa773cf891266bc47b9d5cd73b7
leynad <matei_daniel@outlook.com>
2024-10-19 00:47:46 +0200

    Migrate to express from plain Node.js

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");  // You need to require the path module
+const app = express();
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/contact-me', function(req, res) {
+    res.sendFile(path.join(__dirname, 'contact-me.html'));
+});
+
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
+  })
+
+  console.log('works')
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+
+});
